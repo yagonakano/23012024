b7e38a00f2b2480757cd6ac0c93931152e3da5dc
yagonakano <156016804+yagonakano@users.noreply.github.com>
2024-01-24 14:25:32 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,8 @@
+package controller;
+import view.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Bem vindo a padoka mais deliciosa do mundo!");
+        Cardapio.exibirCardapio();
+    }
+}
